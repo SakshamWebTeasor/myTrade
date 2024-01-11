75bcc9995bb961b87bf710386c99a47f097dfbc2
Saksham Verma <sakshamvwebteasor@gmail.com>
2024-01-11 17:51:20 +0530

    comments removed

--- app/routes/adminRoutes.js
+++ app/routes/adminRoutes.js
@@ -8,17 +8,13 @@ const {
 } = require("../controllers/adminController");
 const router = express.Router();
 
-// Define a GET route
 router.get("/getUsers", getAllUsersC);
 router.get("/myProfile", getMyUserDetailC);
 
-// Define a POST route
 router.post("/user", createUserC);
 
-// Define a Delete route
 router.delete("/user/:id", deleteUserC);
 
-// Define PUT route
 router.put("/user/:id", updateUserC);
 
 module.exports = router;
--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -2,8 +2,4 @@ const express = require("express");
 const {} = require("../controllers/userController");
 const router = express.Router();
 
-// Define a GET route
-
-// Define a POST route
-
 module.exports = router;
